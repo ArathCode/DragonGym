341fdeb429b55fccf325f2ecaaedfcd6bd26cba9
ArathCode <l21240065@smartin.tecnm.mx>
2024-10-15 08:04:57 -0600

    Borrando cosas que no

--- Servidor/borrar_usuario.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include_once("conexion.php");
-if(!empty($_GET['id'])){
-    $clave=$_GET['id'];
-    $consulta=mysqli_query($conexion,"DELETE FROM usuarios where idusuario=$clave");
-    mysqli_close($conexion);
-    header("location:../Cliente/Uusaurios.php");
-}
-?>
